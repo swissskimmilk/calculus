5f038f973bae58dcc8b3d1e6c978da2c51490fbf
Chloe Qiao <qiaochloe@gmail.com>
2022-01-30 10:05:35 -0800

    Minor bug fixes

--- trapT.py
+++ trapT.py
@@ -3,12 +3,12 @@ from  math import *
 # Turns a csv value into an array without whitespace 
 def toArray(csv):
     arr = csv.split(",")
-    arr = [int(x.strip(' ')) for x in arr]
+    arr = [float(eval((x.strip(' ')))) for x in arr]
     return arr
 
 A = float(input("Lower bound (a): "))
 B = float(input("Upper bound (b): "))
-N = float(input("Sub intervals (n): "))
+N = float(input("Subintervals (n): "))
 yRow = input("Enter y row, separated by commas: ")
 
 yArray = toArray(yRow)
@@ -28,10 +28,10 @@ for y in yArray:
     sum += y * multiplier 
 
 # Print out the values being multiplied 
-print(f"Math: {round(multiplier, 2)}[{yArray[0]}", end="")
+print("Math: {}[{}".format(round(multiplier, 2), yArray[0]), end="")
 for i in range(1, len(yArray)): 
-    print(f" + {round(yArray[i], 2)}", end="")
+    print(" + {}".format(round(yArray[i], 2)), end="")
 print("]")
 
 # Print total sum 
-print(f"Total sum: {round(sum, 4)}")
+print("Total sum: {}".format(round(sum, 4)))
