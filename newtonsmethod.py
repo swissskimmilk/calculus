1a677b66cc6be2516ed5a8c932395af7c8b2a05d
Chloe Qiao <qiaochloe@gmail.com>
2022-02-05 00:25:43 -0800

    Fix bug where eval returns imaginary numbers

--- newtonsmethod.py
+++ newtonsmethod.py
@@ -1,17 +1,112 @@
 from math import *
+
+def get_real_root(rt_term, x):
+    root = eval(rt_term, None, {'x':x})
+        
+    if type(root) == complex:
+        
+        if "/" in rt_term:
+            
+            start = rt_term.find("**(") + 3
+            end = len(rt_term)-1
+            
+            base = rt_term[:start-3]
+            fraction = rt_term[start:end]
+            numerator = fraction[:fraction.find("/")]
+            denominator = fraction[fraction.find("/")+1:]
     
-def newtonsmethod():
+            temp_root = eval("{}**{}".format(base, numerator), None, {"x":x})
+            
+            if int(denominator) % 2 == 1 and temp_root < 0:
+                root = eval("{}**(1/{})".format(-temp_root, denominator), None, {"x":x}) * -1
+                return root
+            elif int(denominator) % 2 == 0 and temp_root < 0:
+                return None
+            else:
+                root = eval("{}**(1/{})".format(temp_root, denominator), None, {"x":x})
+                return root
+    else:
+        return root
+
+def solve_eq(eq, x):
+    eq = eq.replace(" ", "")
     
+    index = 0
+    rt_indices = []
+
+    while index < len(eq):
+        index = eq.find('**(', index)
+        if index == -1:
+            break
+        rt_indices.append(index)
+        index += 3 # +3 because len('**(') == 3
+
+    rt_terms = {} # dict of first_index:term
+    rt_ranges = [] # list of first_index:last_index for each term
+
+    for rt_index in rt_indices:
+        rt_term = ""
+        
+        char_before = 0
+        # check for characters before **(
+        for i in range(rt_index-1, -1, -1):
+            if eq[i] in ["+", "-", "*", "/"]:
+                break 
+            else:
+                rt_term = eq[i] + rt_term
+                char_before += 1
+            
+        rt_term = rt_term + "**("
+            
+        char_after = 0
+        # check for characters after **(
+        for i in range(rt_index+3, len(eq)):
+            if eq[i] == ")":
+                rt_term = rt_term + eq[i]
+                char_after += 1
+                break
+            else:
+                rt_term = rt_term + eq[i]
+                char_after += 1
+        
+        rt_ranges.append([rt_index - char_before, rt_index + 3 + char_after])
+        rt_terms[rt_index - char_before] = rt_term
+        
+    for term in rt_terms:
+        rt_terms[term] = get_real_root(rt_terms[term], x)    
+
+    # Create equation_index 
+    # Indices represent individual char or rt_term
+    equation_index = list(range(len(eq)))
+    for rt_range in rt_ranges:
+        count = rt_range[0] # need placeholder index to represent rt equation
+        equation_index.append(count)
+        while count < rt_range[1]:
+            equation_index.remove(count)
+            count += 1
+    equation_index.sort()
+
+    # Create equation string for eval
+    equation = ""
+    for index in equation_index:
+        if index in rt_terms:
+            equation += str(rt_terms[index])
+        else:
+            equation += str(eq[index])
+    
+    output = eval(equation, None, {"x":x})
+    return output
+
+def newtonsmethod(f, df, x0):
     # Change-able variables
     max_iterations = 20
     epsilon = 10**(-6)
     
-    
     # Initial values
     n = 1
     xn = x0
-    fx = eval(f, None, {'x':xn})
-    dfx = eval(df, None, {'x':xn})
+    fx = solve_eq(f, xn)
+    dfx = solve_eq(df, xn)
 
     # Output lists
     n_list, xn_list, fx_list, dfx_list = ["n"], ["xn"], ["fx"], ["dfx"]
@@ -25,8 +120,8 @@ def newtonsmethod():
         dfx_list.append(dfx)
         
         n += 1
-        fx = eval(f, None, {'x':xn})
-        dfx = eval(df, None, {'x':xn})
+        fx = solve_eq(f, xn)
+        dfx = solve_eq(df, xn)
         
     # Format output
     def list_length(lst):
@@ -47,7 +142,6 @@ def newtonsmethod():
                                                 xn_list[i], xn_length + 1,
                                                 fx_list[i], fx_length + 1,
                                                 dfx_list[i], dfx_length + 1))
-        
     if  abs(fx) <= epsilon:
         print("Found solution.")
     elif dfx == 0:
@@ -58,5 +152,4 @@ def newtonsmethod():
 f = input("Function: ")
 df = input("Derivative: ")
 x0 = float(input("Initial x-value: "))
- 
 newtonsmethod(f, df, x0)
